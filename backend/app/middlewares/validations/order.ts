2cffae51429379d8037cb7c9c74226c275fbd74d
Puria Rad <puriajahanbani@gmail.com>
2021-07-22 11:44:11 +0430

    delete unnecessary files

--- backend/app/middlewares/validations/order.ts
+++ backend/app/middlewares/validations/order.ts
@@ -2,40 +2,19 @@ import { isArray } from 'class-validator';
 import validator from 'validator';
 import ErrorHandler from '../error/error';
 
-export class OrderValidation {
-  public static b2cOrder(req, res, next) {
+export class RegisterValidation {
+  public static registerRequest(req, res, next) {
     try {
-      if (!isArray(req.body.cart))
-        throw new ErrorHandler(400, 'سبد شما صحیح نیست.');
-      if (req.body.cart.length <= 0)
-        throw new ErrorHandler(400, 'سبد شما خالی است');
-      if (!req.body.deliveryDate)
-        throw new ErrorHandler(400, 'تاریخ تحویل سفارش الزامی است');
-      if (!req.body.deliveryTime)
-        throw new ErrorHandler(400, 'زمان تحویل سفارش الزامی است');
-      if (req.body.orderInPerson == undefined)
-        throw new ErrorHandler(400, 'تحویل حضوری یا غیر حضوری مشخص نیست');
-
-      next();
-    } catch (err) {
-      next(err);
-    }
-  }
-
-  /* -------------------------------- b2bOrder; ------------------------------- */
-
-  public static b2bOrder(req, res, next) {
-    try {
-      if (!isArray(req.body.cart))
-        throw new ErrorHandler(400, 'سبد شما صحیح نیست.');
-      if (req.body.cart.length <= 0)
-        throw new ErrorHandler(400, 'سبد شما خالی است');
-      if (!req.body.deliveryDate)
-        throw new ErrorHandler(400, 'تاریخ تحویل سفارش الزامی است');
-      if (!req.body.deliveryTime)
-        throw new ErrorHandler(400, 'زمان تحویل سفارش الزامی است');
-      if (req.body.orderInPerson == undefined)
-        throw new ErrorHandler(400, 'تحویل حضوری یا غیر حضوری مشخص نیست');
+      // if (!isArray(req.body.cart))
+      //   throw new ErrorHandler(400, 'سبد شما صحیح نیست.');
+      // if (req.body.cart.length <= 0)
+      //   throw new ErrorHandler(400, 'سبد شما خالی است');
+      // if (!req.body.deliveryDate)
+      //   throw new ErrorHandler(400, 'تاریخ تحویل سفارش الزامی است');
+      // if (!req.body.deliveryTime)
+      //   throw new ErrorHandler(400, 'زمان تحویل سفارش الزامی است');
+      // if (req.body.orderInPerson == undefined)
+      //   throw new ErrorHandler(400, 'تحویل حضوری یا غیر حضوری مشخص نیست');
 
       next();
     } catch (err) {
